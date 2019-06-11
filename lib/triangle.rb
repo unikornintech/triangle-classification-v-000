83e64a9e1c579795149b622f42a9c2d8d3bfe792
Lauren Days <lmchambers0420@gmail.com>
2019-06-11 20:36:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :s1, :s2, :s3
+  
+  attr_reader :sides 
+  
+  def initialize(s1, s2, s3)
+    @sides = [s1, s2, s3].sort 
+    @s1 = sides[1]
+    @s2 = sides[2]
+    @s3 = sides[3]
+    
+  end 
+  
+  def kind 
+    
+    
+  end 
+  
+  class TriangleError < StandardError
+    
+  end 
 end
